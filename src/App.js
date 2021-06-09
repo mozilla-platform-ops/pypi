b72ac4fc7c30ba523b51b4524e44403fc48202ed
Rob Thijssen <rthijssen@gmail.com>
2021-06-09 11:54:07 +0300

    links to packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import React, { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
 
 function App() {
+  const [packageUrls, setPackageUrls] = useState([]);
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/mozilla-platform-ops/pypi/main/packageUrls.json')
+      .then(response => response.json())
+      .then(content => setPackageUrls(content))
+      .catch(e => console.error(e))
+  }, []);
+
   return (
-    <Container></Container>
+    <Container>
+      <ul>
+        {
+          packageUrls.map((packageUrl) => (
+            <li key={packageUrl}>
+              <a href={packageUrl}>{packageUrl.split('/').pop()}</a>
+            </li>
+          ))
+        }      
+      </ul>
+    </Container>
   );
 }
 
