b0e2d5a922013f6ce3c6232719d437e8395c431b
Rob Thijssen <rthijssen@gmail.com>
2021-06-09 12:34:20 +0300

    instructions

---
+++ public/readme.md
@@ -0,0 +1,14 @@
+# mozilla-platform-ops/pypi
+
+this page hosts links to python packages.
+
+it can be used by pip like so:
+
+```bash
+pip install ${package} --no-index --find-links https://mozilla-platform-ops.github.io/pypi
+
+```
+
+to add a package link, edit [packageUrls.json](https://github.com/mozilla-platform-ops/pypi/edit/main/packageUrls.json).
+
+---
--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from "react";
 import Container from 'react-bootstrap/Container';
+import ReactMarkdown from 'react-markdown';
 
 function App() {
+  const [readme, setReadme] = useState('');
   const [packageUrls, setPackageUrls] = useState([]);
   useEffect(() => {
+    fetch('readme.md')
+      .then(response => response.text())
+      .then(content => setReadme(content))
+      .catch(e => console.error(e));
     fetch('https://raw.githubusercontent.com/mozilla-platform-ops/pypi/main/packageUrls.json')
       .then(response => response.json())
-      .then(content => setPackageUrls(content))
-      .catch(e => console.error(e))
+      .then(content => {
+        content.sort((a, b) => {
+          if (a.split('/').pop() < b.split('/').pop()) {
+            return -1;
+          }
+          if (a.split('/').pop() > b.split('/').pop()) {
+            return 1;
+          }
+          return 0;
+        });
+        setPackageUrls(content);
+      })
+      .catch(e => console.error(e));
   }, []);
 
   return (
     <Container>
+      <ReactMarkdown>{readme}</ReactMarkdown>
       <ul>
         {
           packageUrls.map((packageUrl) => (
